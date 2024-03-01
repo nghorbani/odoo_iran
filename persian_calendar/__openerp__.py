f8ec17d0989f52a7cfb4bee933c9ddc5eddb72c0
Nima Ghorbani <7373673+nghorbani@users.noreply.github.com>
2024-03-01 21:49:49 +0100

    Update __openerp__.py

--- persian_calendar/__openerp__.py
+++ persian_calendar/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Copyright (c) 2010-2013, Nima Ghorbani , All Rights Reserved
-#        Nima ghorbani <nimaqzzz@gmail.com> <http://ir.linkedin.com/in/nimaghi>
+#        Nima ghorbani <https://www.linkedin.com/in/nghorbani/>
 #        Developed Exclusively For SuperPipe International Company <http://www.superpipe.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -35,7 +35,7 @@ along with persian date-times.
     """,
     'summary': 'Persian Calendar Support for OpenERP',
     'author': 'Nima Ghorbani',
-    'website': 'http://ir.linkedin.com/in/nimagbani',
+    'website': 'https://www.linkedin.com/in/nghorbani/',
     'depends': ['base','web'],
     'init_xml': [],
     'data': [
